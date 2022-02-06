a95fde7ecf92e1e8801f056f81b0c61bb22ca9b0
fuckmylife <98694702+fuxkmylife@users.noreply.github.com>
2022-02-06 17:21:29 +0800

    Add files via upload

---
+++ precache-manifest.f9969861f30abea85b340ee428d002a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b9f283300c0d774f1ef6db3fecdfea",
+    "url": "/starcatchers_catchgame/index.html"
+  },
+  {
+    "revision": "40b2799a9a76d3f1a33f",
+    "url": "/starcatchers_catchgame/static/css/2.f1bd3b54.chunk.css"
+  },
+  {
+    "revision": "663bb44afa67e52c6a8c",
+    "url": "/starcatchers_catchgame/static/css/main.15c5ed9c.chunk.css"
+  },
+  {
+    "revision": "40b2799a9a76d3f1a33f",
+    "url": "/starcatchers_catchgame/static/js/2.7372b25b.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/starcatchers_catchgame/static/js/2.7372b25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "663bb44afa67e52c6a8c",
+    "url": "/starcatchers_catchgame/static/js/main.6e42f2dc.chunk.js"
+  },
+  {
+    "revision": "4e46aa7f6bb0b6d941d6",
+    "url": "/starcatchers_catchgame/static/js/runtime-main.b91f0c8e.js"
+  },
+  {
+    "revision": "7f2ccb9a74c5c1430ec445cba4cb5f7f",
+    "url": "/starcatchers_catchgame/static/media/SC_game_catchstar-03-min.7f2ccb9a.png"
+  },
+  {
+    "revision": "69d925d6158255d82099197514714d9e",
+    "url": "/starcatchers_catchgame/static/media/SC_game_catchstar-04-min.69d925d6.png"
+  },
+  {
+    "revision": "9597e80e13c00aeb2b0ac2e8c3c5e4b4",
+    "url": "/starcatchers_catchgame/static/media/SC_game_catchstar-05-min.9597e80e.png"
+  },
+  {
+    "revision": "2a30c9ae18afdd098094fed3e2b92620",
+    "url": "/starcatchers_catchgame/static/media/SC_game_catchstar-06-min.2a30c9ae.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/starcatchers_catchgame/precache-manifest.f9969861f30abea85b340ee428d002a3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starcatchers_catchgame/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
